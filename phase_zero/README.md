4b29c86809501e19a3be3db0d018dfa92fea54ce
Robert <52116466+RobertTaylor94@users.noreply.github.com>
2023-11-14 10:52:27 +0000

    Delete phase_zero directory

--- phase_zero/README.md
+++
@@ -1,7 +0,0 @@
-# Phase Zero
-
-- [x] Testing functions with equality
-
-- [x] Testing classes with equality
-
-- [ ] Testing for errors
--- phase_zero/tests/test_greet.py
+++
@@ -1,5 +0,0 @@
-from lib.greet import *
-
-def test_greet_returns_name():
-    result = greet('Sheila')
-    assert result == "Hello, Sheila!"
